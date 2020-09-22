f0673fe936fe1d4ccbce397cf6773134b9a04ed9
khj <jhk7238@gmail.com>
2020-09-23 01:26:07 +0900

    standalone

---
+++ src/hello.c
@@ -0,0 +1,9 @@
+/* Copyright 2019 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, SemiFive!\n");
+}
